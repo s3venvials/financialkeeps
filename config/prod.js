8a2a91e2235f47a2b43eee2c47efaf37f4f033df
testcerium <testcerium.io@gmail.com>
2019-12-23 20:28:56 -0700

    Updating prod origin url.

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,16 @@ module.exports = {
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
-    redirectDomain: process.env.REDIRECT_DOMAIN
+    redirectDomain: process.env.REDIRECT_DOMAIN,
+    "corsOptions": {
+      "credentials": true,
+      "origin": [
+        "https://financialkeeps.herokuapp.com/",
+        "https://financialkeeps.herokuapp.com/bills/new",
+        "https://financialkeeps.herokuapp.com/new/profile"
+      ],
+      "methods": "GET,POST",
+      "preflightContinue": true,
+      "optionsSuccessStatus": 202
+    }
   };
